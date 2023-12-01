1664791a7e60849930cc138122d7f78c6019ce62
kenta.I <kenta1205i@outlook.jp>
2023-12-01 13:10:20 +0900

    ペナルティ値によって異なる音流す関数

--- app/src/main/java/com/example/alarmtestjava/MainActivity.java
+++ app/src/main/java/com/example/alarmtestjava/MainActivity.java
@@ -9,22 +9,42 @@ import android.view.View;
 public class MainActivity extends AppCompatActivity {
 
     private MediaPlayer alarm;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        alarm = MediaPlayer.create(this,R.raw.alarm);
+        //alarm = MediaPlayer.create(this,R.raw.alarm);
+        alarm = MediaPlayer.create(this, soundResourceID(2));
     }
 
-    /* スタートボタン */
-    public void onStart(View view){
-        alarm.start();
+    public static int soundResourceID(int penaltyValue) {
+
+        // 適切な soundResourceID を設定
+        switch (penaltyValue) {
+            case 1:
+                //soundResourceID = R.raw.alarm1;
+                return R.raw.alarm1;
+            //break;
+            //case 2:
+            //  soundResourceID = R.raw.alarm2;
+            //break;
+            // 必要に応じて他のケースを追加
+            default:
+                //soundResourceID = R.raw.alarm;
+                return R.raw.alarm;
+            //break;
+        }
     }
 
-    /* ストップボタン */
-    public void onStop(View view){
-        alarm.stop();
-    }
+        /* スタートボタン */
+        public void onStart (View view){
+            alarm.start();
+        }
+
+        /* ストップボタン */
+        public void onStop (View view){
+            alarm.stop();
+        }
+
 }
