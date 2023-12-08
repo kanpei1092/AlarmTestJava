f2e92cae09f45b7c2ff0c61039c44841e9694ee0
Ichikawa Kodai <cmt14006@ict.nitech.ac.jp>
2023-12-08 14:20:50 +0900

    音を鳴らしたい

--- app/src/main/java/com/example/alarmtestjava/AlarmReceiver.java
+++ app/src/main/java/com/example/alarmtestjava/AlarmReceiver.java
@@ -3,6 +3,7 @@ package com.example.alarmtestjava;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.util.Log;
 
 import androidx.core.app.NotificationCompat;
@@ -16,6 +17,8 @@ public class AlarmReceiver extends BroadcastReceiver {
         showNotification(context, "Alarm", "Wake up! It's time!");
 
         // ここでアラームが鳴ったときに行いたい処理を追加できます
+        MediaPlayer alarm = MediaPlayer.create(this, R.raw.alarm);
+        alarm.start();
     }
 
     private void showNotification(Context context, String title, String content) {
