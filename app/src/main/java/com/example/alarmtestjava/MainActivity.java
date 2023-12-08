51b4f42009380fb54cf795189ecf9e33393d3802
Ichikawa Kodai <cmt14006@ict.nitech.ac.jp>
2023-12-08 13:52:22 +0900

    アラーム機能完成(多分)

--- app/src/main/java/com/example/alarmtestjava/MainActivity.java
+++ app/src/main/java/com/example/alarmtestjava/MainActivity.java
@@ -2,13 +2,17 @@ package com.example.alarmtestjava;
 
 import android.app.AlarmManager;
 import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
@@ -31,11 +35,21 @@ public class MainActivity extends AppCompatActivity {
 
         alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
         timePicker = findViewById(R.id.timePicker);
+        Log.d("MainActivity", "get timePicker!");
+
+        Button setAlarmButton = findViewById(R.id.setAlarmButton);
+        setAlarmButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setAlarm(v);
+            }
+        });
     }
 
     public void setAlarm(View view) {
         int hour = timePicker.getHour();
         int minute = timePicker.getMinute();
+        Log.d("MainActivity", "get time!");
 
         Calendar calendar = Calendar.getInstance();
         calendar.set(Calendar.HOUR_OF_DAY, hour);
@@ -43,12 +57,29 @@ public class MainActivity extends AppCompatActivity {
         calendar.set(Calendar.SECOND, 0);
 
         setAlarmNotification(calendar.getTimeInMillis());
+        Log.d("MainActivity", "set!");
+    }
+
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "Default Channel";
+            String description = "Default Channel Description";
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel channel = new NotificationChannel("default_channel", name, importance);
+            channel.setDescription(description);
+
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
     }
 
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     private void setAlarmNotification(long triggerTime) {
+        createNotificationChannel(); // 通知チャネルを作成
+
+
         Intent intent = new Intent(this, AlarmReceiver.class);
-        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0, intent, PendingIntent.FLAG_IMMUTABLE);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 1, intent, PendingIntent.FLAG_IMMUTABLE);
 
         alarmManager.setExact(AlarmManager.RTC_WAKEUP, triggerTime, pendingIntent);
 
