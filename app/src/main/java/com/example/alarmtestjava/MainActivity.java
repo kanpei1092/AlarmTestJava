e8226d8e07c2ac48f9a1997b7ff6a7a12d8e42b7
Ichikawa Kodai <cmt14006@ict.nitech.ac.jp>
2023-12-29 16:20:07 +0900

    soundVolumeの修正

--- app/src/main/java/com/example/alarmtestjava/MainActivity.java
+++ app/src/main/java/com/example/alarmtestjava/MainActivity.java
@@ -54,7 +54,7 @@ public class MainActivity extends AppCompatActivity {
 
             //NFCが利用可能か表示
             if (nfcAdapter == null) {
-                Toast.makeText(this, "NFCは利用できません", Toast.LENGTH_LONG).show();
+                //Toast.makeText(this, "NFCは利用できません", Toast.LENGTH_LONG).show();
             } else {
                 //Toast.makeText(this, "NFCは利用できます", Toast.LENGTH_LONG).show();
                 // スキャンされたときにタグの詳細情報でPendingIntentオブジェクトを準備するようにAndroidシステムに指示する
